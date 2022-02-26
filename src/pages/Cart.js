ed31405f96123bf4dd8230d57151c7e41f500557
Akash Kumar <akashkumar59200@gmail.com>
2022-02-26 22:09:20 +0530

    proxy removed

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -16,7 +16,7 @@ const Cart = () => {
     if(priceFetched) {
       return;
     }
-    fetch('/api/products/cart-items', {
+    fetch('https://ecom-rest-apis.herokuapp.com/api/products/cart-items', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json'
